9f271015e9bda05352164d8f1b2236f1d6c29042
zlp <zlp@umich.edu>
2021-10-12 13:34:10 -0400

    Newcommit

--- regex.py
+++ regex.py
@@ -23,34 +23,35 @@ def find_word(string_list):
     """ Return a list of words that contain three digit numbers in the middle. """
 
     # initialize an empty list
-
+    results = []
     # define the regular expression
 
     # loop through each line of the string list 
-
+    for line in string_list:
     # find all the words that match the regular expression in each line
-    
+        line = line.rstrip()
+        x = re.findall('\w+[0,9]{3},\w+', line)
     # loop through the found words and add the words to your empty list 
 
     #return the list of all words that start with the letter B, E, or T
-    pass
+    return results
 
 
 def find_days(string_list):
     """ Return a list of days from the list of strings the dates format in the text are MM/DD/YYYY. """  
 
     # initialize an empty list
-
+    results = []
     # define the regular expression
-
+    
     # loop through each line of the string list
-    
+    for line in string_list:
     # find all the dates that match the regular expression in each line
-    
+        print('hello')
     # loop through the found dates and only add the days to your empty list 
     
     #return the list of days
-    pass
+    return results
 
 def find_domains(string_list):
     """ Return a list of web address domains from the list of strings the domains of a wbsite are after www. """
